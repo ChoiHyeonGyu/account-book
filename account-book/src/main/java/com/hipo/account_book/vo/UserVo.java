054b5a567de1113f28c3d838c73e54521f6e7cbc
dongjune <melfs363@gmail.com>
2017-04-21 14:58:51 +0900

    UserVo added toString()

--- account-book/src/main/java/com/hipo/account_book/vo/UserVo.java
+++ account-book/src/main/java/com/hipo/account_book/vo/UserVo.java
@@ -9,62 +9,75 @@ public class UserVo {
 	private int age;
 	private String photo;
 	private int total;
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	
+
 	public String getBirth() {
 		return birth;
 	}
+
 	public void setBirth(String birth) {
 		this.birth = birth;
 	}
-	
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+
 	public String getPhoto() {
 		return photo;
 	}
+
 	public void setPhoth(String photo) {
 		this.photo = photo;
 	}
-	
+
 	public int getTotal() {
 		return total;
 	}
+
 	public void setTotal(int total) {
 		this.total = total;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return "UserVo [id=" + id + ", password=" + password + ", name=" + name + ", gender=" + gender + ", birth="
+				+ birth + ", age=" + age + ", photo=" + photo + ", total=" + total + "]";
+	}
+
 }
