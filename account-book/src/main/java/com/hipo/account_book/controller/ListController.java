15375747096cd10948c4ff4758a879aa11297f5c
jason <jason@DESKTOP-4D097B8>
2017-04-24 15:01:43 +0900

    aa

--- account-book/src/main/java/com/hipo/account_book/controller/ListController.java
+++ account-book/src/main/java/com/hipo/account_book/controller/ListController.java
@@ -13,29 +13,34 @@ import com.hipo.account_book.service.ListService;
 import com.hipo.account_book.vo.ListVo;
 
 @Controller
+@RequestMapping("/{id}")
 public class ListController {
 	@Autowired
 	private ListService service;
 
-	@RequestMapping("/{id}/main")
+	@RequestMapping("/main")
 	public String List(Model model ,@ModelAttribute ListVo vo) {
 		List<ListVo> list = service.getList(vo);
 		model.addAttribute("list", list);
 		return "/main";
 	}
-	@RequestMapping("/{id}/listdelete")
+	@RequestMapping("/listdelete")
 	public String List(@PathVariable String id,@ModelAttribute ListVo vo){
 		 service.delete(vo);
 		return "/main";
 		
 	}
-	@RequestMapping("/{id}/add")
+	@RequestMapping("/add")
 	public String add(@ModelAttribute ListVo vo,@PathVariable String id){
-		List<ListVo> list = service.getList(vo);
 		System.out.println("더하는 부분 아이디 잘가지고 옵니다" + vo);
+		String list = service.add(vo);
 		
-		service.add(vo);
+		return "/main";
+		
+	}
+	@RequestMapping("/modify")
+	public String modify(@ModelAttribute ListVo vo,@PathVariable String id){
+		
 		return null;
-		
 	}
 }
