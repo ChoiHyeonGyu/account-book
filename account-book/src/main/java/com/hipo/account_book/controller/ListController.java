8d4af4d32602154a479160283b94457cd213e345
jason <jason@DESKTOP-4D097B8>
2017-04-28 11:07:43 +0900

    dsad

--- account-book/src/main/java/com/hipo/account_book/controller/ListController.java
+++ account-book/src/main/java/com/hipo/account_book/controller/ListController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.javassist.tools.reflect.Sample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +12,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hipo.account_book.dto.JSONResult;
 import com.hipo.account_book.service.ListService;
 import com.hipo.account_book.service.OptionService;
 import com.hipo.account_book.vo.BoardVo;
+import com.hipo.account_book.vo.ListPagingVo;
 import com.hipo.account_book.vo.ListVo;
 import com.hipo.account_book.vo.OptionVo;
 
@@ -73,7 +77,24 @@ public class ListController {
 		return "redirect:/"+id+"/main";
 		
 	}
-	
+	@RequestMapping(value="/list", method=RequestMethod.GET)
+	public ModelAndView list(Sample sample) throws Exception {
+	    try {
+	        // (Before) Doing...
+
+	        ListPagingVo paging = new ListPagingVo();
+	        paging.setPageNo(1);
+	        paging.setPageSize(10);
+	        paging.setTotalCount(10);
+
+
+	        // (After) Doing...
+	    } catch (Exception e) {
+	        throw e;
+	    }
+		return null;
+	}
+
 	@RequestMapping("/boardadd")
 	public String boardadd(@PathVariable String id, @ModelAttribute BoardVo boardvo, @RequestParam("file") List<MultipartFile> file){
 		service.boardadd(id, boardvo, file);
--- account-book/webapp/WEB-INF/views/list.jsp
+++ account-book/webapp/WEB-INF/views/list.jsp
@@ -135,7 +135,27 @@
 								<td><button id="myBtn">추가하기</button> </td>	
 							<td><a href="${pageContext.request.contextPath}">맵보기!!!!!</a></td>
 						</tr>
+						
 					</c:forEach>
+	
+<tr class="paginate">
+    <a href="javascript:goPage(${param.firstPageNo})" class="first">처음 페이지</a>
+    <a href="javascript:goPage(${param.prevPageNo})" class="prev">이전 페이지</a>
+    <span>
+        <c:forEach var="i" begin="${param.startPageNo}" end="${param.endPageNo}" step="1">
+            <c:choose>
+                <c:when test="${i eq param.pageNo}"><a href="javascript:goPage(${i})" class="choice">${i}</a></c:when>
+                <c:otherwise><a href="javascript:goPage(${i})">${i}</a></c:otherwise>
+            </c:choose>
+        </c:forEach>
+    </span>
+    <a href="javascript:goPage(${param.nextPageNo})" class="next">다음 페이지</a>
+    <a href="javascript:goPage(${param.finalPageNo})" class="last">마지막 페이지</a>
+</tr>
+
+
+
+
 			<p>
 			<div id="modify" title="수정하기" style="display:none" >
 				<form id="modify11"  action="${pageContext.request.contextPath }/${currentuserid}/modify1" method="post">
