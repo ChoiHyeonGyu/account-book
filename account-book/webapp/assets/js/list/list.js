b8e9b0bf2f4c596c6709efc4f9ecaf6b9327ebb8
jason <jason@DESKTOP-4D097B8>
2017-05-22 17:52:46 +0900

    final list

--- account-book/webapp/assets/js/list/list.js
+++ account-book/webapp/assets/js/list/list.js
@@ -14,6 +14,15 @@ $(function(){
 		}
 	});
 		
+
+	$( "#datepicker" ).datepicker({
+			dateFormat: 'yymmdd',
+			monthNamesShort:['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+			dayNamesMin:['일','월','화','수','목','금','토'],
+			changeMonth: true,
+			changeYear:true,
+			showMonthAfterYear:true
+		});
 /*	var myBtnform1 = $("#modify").dialog({
 		autoOpen: false,
 		height: 800,
