660270db98b87a88f8c33b7ff8488c63e861aece
dongjune <melfs363@gmail.com>
2017-05-31 18:00:46 +0900

    androidchartcategory ing...

--- account-book/src/main/java/com/hipo/account_book/androidcontroller/AndroidChartController.java
+++ account-book/src/main/java/com/hipo/account_book/androidcontroller/AndroidChartController.java
@@ -1,25 +1,30 @@
 package com.hipo.account_book.androidcontroller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hipo.account_book.service.AndroidChartService;
+import com.hipo.account_book.vo.AndroidCategoryChartVo;
 
 @Controller
 @RequestMapping("/android/{id}")
 public class AndroidChartController {
-	
+
 	@Autowired
 	private AndroidChartService aChartService;
 
 	@RequestMapping("findCategory")
-	public void findCategory(@PathVariable String id) {
+	@ResponseBody
+	public List<AndroidCategoryChartVo> findCategory(@PathVariable String id) {
 		Map<String, String> categoryMap = aChartService.findCategory(id);
-		aChartService.findCategory(categoryMap,id);
+		return aChartService.findCategory(categoryMap, id);
+
 	}
 
 }
