cec9aad660bac9ea95bfe4f2ee577dc8a0346aaf
bit-user <bit-user@DESKTOP-3E14R52>
2017-06-02 10:31:02 +0900

    맵 마커 찍힘

--- account-book/webapp/assets/js/list/list.js
+++ account-book/webapp/assets/js/list/list.js
@@ -264,34 +264,41 @@ var currmap = function(){
 			
 		}
 	});
+	
+	var myLatlng = function(){};
+	var mapOptions = {};
+	var map = function(){};
+	var marker = function(){};
+	var infowindow = function(){};
+	
+	var Y_point = 37.494622; // Y 좌표
+	var X_point = 127.027610; // X 좌표
+	var zoomLevel = 18; // 지도의 확대 레벨 : 숫자가 클수록 확대정도가 큼
+	var markerTitle = "사용한 위치를 찍어보세요"; // 현재 위치 마커에 마우스를 오버올 때 나타나는 정보
+	var markerMaxWidth = 300; // 마커를 클릭했을 때 나타나는 말풍선의 최대 크기
+	
+	// 말풍선 내용
+	var contentString = '<div>사용한 위치를 찍어보세요</div>';
 		
 	$("#btnmap").click(function(){
 		if(again == false){
-			var Y_point = 37.494622; // Y 좌표
-			var X_point = 127.027610; // X 좌표
-			var zoomLevel = 18; // 지도의 확대 레벨 : 숫자가 클수록 확대정도가 큼
-			var markerTitle = "사용한 위치를 찍어보세요"; // 현재 위치 마커에 마우스를 오버올 때 나타나는 정보
-			var markerMaxWidth = 300; // 마커를 클릭했을 때 나타나는 말풍선의 최대 크기
-			
-			// 말풍선 내용
-			var contentString = '<div>사용한 위치를 찍어보세요</div>';
-			
-			var myLatlng = new google.maps.LatLng(Y_point, X_point);
-			var mapOptions = {
+			myLatlng = new google.maps.LatLng(Y_point, X_point);
+			mapOptions = {
 				zoom: zoomLevel,
 				center: myLatlng,
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			}
-			var map = new google.maps.Map(document.getElementById('map_ma'), mapOptions);
-			var marker = new google.maps.Marker({
+			map = new google.maps.Map(document.getElementById('map_ma'), mapOptions);
+			marker = new google.maps.Marker({
 				position: myLatlng,
 				map: map,
 				title: markerTitle
 			});
-			var infowindow = new google.maps.InfoWindow({
+			infowindow = new google.maps.InfoWindow({
 				content: contentString,
 				maxWizzzdth: markerMaxWidth
 			});
+			
 			google.maps.event.addListener(marker, 'click', function(){
 				infowindow.open(map, marker);
 			});
@@ -301,9 +308,18 @@ var currmap = function(){
 				console.log(mouseEvent.latLng.lng());
 				$("#lat").val(mouseEvent.latLng.lat());
 				$("#lng").val(mouseEvent.latLng.lng());
+				Y_point = mouseEvent.latLng.lat();
+				X_point = mouseEvent.latLng.lng();
+				myLatlng = new google.maps.LatLng(Y_point, X_point);
+				marker = new google.maps.Marker({
+					position: myLatlng,
+					map: map,
+					title: markerTitle
+				});
+				
+				infowindow.open(map, marker);
 			});
 		}
-		console.log(again);
 		mapform.dialog("open");
 		again = true;
 	});
